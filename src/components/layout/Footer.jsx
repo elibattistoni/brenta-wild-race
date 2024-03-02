8545ba19903acdf908b5ef31206abe26f08ec92a
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-02 20:47:02 +0100

    feat(footer): add footer - work in progress

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,112 @@
+import { Text, Container, ActionIcon, Group, rem } from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+  IconBrandFacebookFilled,
+  IconBrandFacebook,
+} from "@tabler/icons-react";
+import classes from "./Footer.module.css";
+
+const data = [
+  {
+    title: "About",
+    links: [
+      { label: "Features", link: "#" },
+      { label: "Pricing", link: "#" },
+      { label: "Support", link: "#" },
+      { label: "Forums", link: "#" },
+    ],
+  },
+  {
+    title: "Project",
+    links: [
+      { label: "Contribute", link: "#" },
+      { label: "Media assets", link: "#" },
+      { label: "Changelog", link: "#" },
+      { label: "Releases", link: "#" },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      { label: "Join Discord", link: "#" },
+      { label: "Follow on Twitter", link: "#" },
+      { label: "Email newsletter", link: "#" },
+      { label: "GitHub discussions", link: "#" },
+    ],
+  },
+];
+
+const Footer = () => {
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          {/* <MantineLogo size={30} /> */}
+          {/* <Text size="xs" c="dimmed" className={classes.description}>
+            Build fully functional accessible web applications faster than ever
+          </Text> */}
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © 2024 - www.brentawildrace.it - Associazione Sportiva K40 - 38010 -
+          Campodenno (TN) - Tutti i diritti riservati
+        </Text>
+
+        <Group
+          gap={0}
+          className={classes.social}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon
+            size="lg"
+            color="var(--mantine-color-orangeBrenta-0)"
+            variant="subtle"
+          >
+            <IconBrandFacebook
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon
+            size="lg"
+            color="var(--mantine-color-orangeBrenta-0)"
+            variant="subtle"
+          >
+            <IconBrandInstagram
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "./MainNavigation";
+import Footer from "./Footer";
 
 const RootLayout = () => {
   return (
@@ -8,6 +9,7 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };

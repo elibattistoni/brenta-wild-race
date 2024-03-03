cfd22cd4b40022217e2fc2db7d1f95dabbdd384e
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-03 11:00:06 +0100

    feat(footer): add footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,60 +1,24 @@
 import { Text, Container, ActionIcon, Group, rem } from "@mantine/core";
-import {
-  IconBrandTwitter,
-  IconBrandYoutube,
-  IconBrandInstagram,
-  IconBrandFacebookFilled,
-  IconBrandFacebook,
-} from "@tabler/icons-react";
+import { IconBrandInstagram, IconBrandFacebook } from "@tabler/icons-react";
 import classes from "./Footer.module.css";
-
-const data = [
-  {
-    title: "About",
-    links: [
-      { label: "Features", link: "#" },
-      { label: "Pricing", link: "#" },
-      { label: "Support", link: "#" },
-      { label: "Forums", link: "#" },
-    ],
-  },
-  {
-    title: "Project",
-    links: [
-      { label: "Contribute", link: "#" },
-      { label: "Media assets", link: "#" },
-      { label: "Changelog", link: "#" },
-      { label: "Releases", link: "#" },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      { label: "Join Discord", link: "#" },
-      { label: "Follow on Twitter", link: "#" },
-      { label: "Email newsletter", link: "#" },
-      { label: "GitHub discussions", link: "#" },
-    ],
-  },
-];
+import { footerPaths } from "../../utils/paths";
+import { NavLink } from "react-router-dom";
+// import { ReactComponent as BrentaWRLogo } from "../../assets/brentawildracelogo.svg";
+import BrentaWRLogo from "../../assets/brentawildracelogo.svg?react";
 
 const Footer = () => {
-  const groups = data.map((group) => {
-    const links = group.links.map((link, index) => (
-      <Text
-        key={index}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-    ));
+  const footerLinks = footerPaths.map((group) => {
+    const links = group.links.map((link, idx) => {
+      return (
+        <NavLink key={idx} to={link.link} className={classes.link}>
+          {link.title}
+        </NavLink>
+      );
+    });
 
     return (
       <div className={classes.wrapper} key={group.title}>
-        <Text className={classes.title}>{group.title}</Text>
+        <span className={classes.title}>{group.title}</span>
         {links}
       </div>
     );
@@ -63,13 +27,8 @@ const Footer = () => {
   return (
     <footer className={classes.footer}>
       <Container className={classes.inner}>
-        <div className={classes.logo}>
-          {/* <MantineLogo size={30} /> */}
-          {/* <Text size="xs" c="dimmed" className={classes.description}>
-            Build fully functional accessible web applications faster than ever
-          </Text> */}
-        </div>
-        <div className={classes.groups}>{groups}</div>
+        <BrentaWRLogo className={classes.logo} />
+        {footerLinks}
       </Container>
       <Container className={classes.afterFooter}>
         <Text c="dimmed" size="sm">

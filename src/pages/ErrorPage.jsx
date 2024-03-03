bb9378d4334f5e4cc19e9d4f263546f756e8c7ae
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-03 08:30:37 +0100

    feat(error): add error handling for EditionDetailsPage and better ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,19 @@
+import { useRouteError } from "react-router-dom";
+import Footer from "../components/layout/Footer";
+import MainNavigation from "../components/layout/MainNavigation";
+
 const ErrorPage = () => {
-  return <div>ErrorPage -- under construction</div>;
+  const error = useRouteError();
+
+  return (
+    <>
+      <MainNavigation />
+      <div>ErrorPage -- under construction</div>
+      <div>{error.status}</div>
+      <div>{error.statusText}</div>
+      <Footer />
+    </>
+  );
 };
 
 export default ErrorPage;
--- src/pages/editions/EditionDetailsPage.jsx
+++ src/pages/editions/EditionDetailsPage.jsx
@@ -2,7 +2,12 @@ import { useParams } from "react-router-dom";
 
 const EditionDetailsPage = () => {
   const { year } = useParams();
-  return <div>EditionDetailsPage -- {year} -- under construction</div>;
+
+  if (year === "2017" || year === "2018" || year === "2019") {
+    return <div>EditionDetailsPage -- {year} -- under construction</div>;
+  } else {
+    throw new Response(null, { status: 404, statusText: "Not Found" });
+  }
 };
 
 export default EditionDetailsPage;

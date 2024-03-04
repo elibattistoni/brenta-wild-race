6ab4591779cac71786df530027b86ebaf63525fb
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-04 11:04:18 +0100

    feat(heading): fix heading component with small padding on left to make it aligned with title

--- src/components/shared/Heading.jsx
+++ src/components/shared/Heading.jsx
@@ -1,10 +1,12 @@
-import { Container, Title } from "@mantine/core";
+import { Container, Title, rem } from "@mantine/core";
 
 const Heading = ({ subtitle, title }) => {
   return (
     <Container fluid mt="xl">
       <header>
-        <Title order={5}>{subtitle}</Title>
+        <Title order={5} pl={rem(2)}>
+          {subtitle}
+        </Title>
         <Title order={2} c="orangeBrenta.0">
           {title}
         </Title>

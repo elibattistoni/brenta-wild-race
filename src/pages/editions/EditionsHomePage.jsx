8b44f9f57e2659a2f2b1b3fe31f7871399087cc8
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-04 11:08:50 +0100

    feat(editionspage): add links to specific pages for each year

--- src/pages/editions/EditionsHomePage.jsx
+++ src/pages/editions/EditionsHomePage.jsx
@@ -1,12 +1,27 @@
+import { NavLink } from "react-router-dom";
 import Heading from "../../components/shared/Heading";
-import { Container, Text } from "@mantine/core";
+import { Container, Stack, Text } from "@mantine/core";
+import classes from "./EditionsHomePage.module.css";
 
 const EditionsHomePage = () => {
   return (
     <>
       <Heading title="EDIZIONI PASSATE" subtitle="EDIZIONI" />
       <Container fluid mt="xl">
-        <Text size="xl">Pagina in costruzione. Stay Tuned!</Text>
+        <Text size="xl" mb="xl">
+          Pagina in costruzione. Stay Tuned!
+        </Text>
+        <Stack>
+          <NavLink to="/edizioni/2017" className={classes.link}>
+            Edizione 2017
+          </NavLink>
+          <NavLink to="/edizioni/2018" className={classes.link}>
+            Edizione 2018
+          </NavLink>
+          <NavLink to="/edizioni/2019" className={classes.link}>
+            Edizione 2019
+          </NavLink>
+        </Stack>
       </Container>
     </>
   );

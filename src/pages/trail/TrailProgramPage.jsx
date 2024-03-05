1ba5c0ca11b5e009aa6b7e004104a2d038a81680
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-05 11:35:35 +0100

    feat(programpage): make it responsive

--- src/pages/trail/TrailProgramPage.jsx
+++ src/pages/trail/TrailProgramPage.jsx
@@ -5,18 +5,30 @@ import {
   Stack,
   Text,
   Title,
+  em,
   rem,
-  useMantineTheme,
 } from "@mantine/core";
 import MountainDrawing from "../../assets/program-mountain.svg?react";
 import classes from "./TrailProgramPage.module.css";
 import Heading from "../../components/shared/Heading";
+import { fontSizesTitles, fontWeights } from "../../mantineUITheme";
+import { useMediaQuery } from "@mantine/hooks";
 
 const ProgramDayDate = ({ day, date }) => {
   return (
     <Stack align="center" gap={0}>
       <Text>{day}</Text>
-      <Title order={4} c="bluePrimary.0">
+      {/* <Title order={4} c="bluePrimary.0" className={classes.titleDate}> */}
+      <Title
+        order={4}
+        fz={{
+          base: fontSizesTitles.fontSizeh6_32px,
+          sm: fontSizesTitles.fontSizeh5_36px,
+          md: fontSizesTitles.fontSizeh4_40px,
+        }}
+        fw={fontWeights.medium}
+        c="bluePrimary.0"
+      >
         {date}
       </Title>
     </Stack>
@@ -24,59 +36,65 @@ const ProgramDayDate = ({ day, date }) => {
 };
 
 const HourScheduleGroup = ({ hour, text }) => {
-  const theme = useMantineTheme();
   return (
     <Stack align="center" gap={0}>
-      <Text size="xl" fw={theme.other.fontWeights.semiBold}>
+      <Text
+        fz={{ base: "md", sm: "lg", md: "xl" }}
+        fw="var(--mantine-fw-semibold)"
+      >
         {hour}
       </Text>
-      <Text size="lg">{text}</Text>
+      <Text fz={{ base: "sm", sm: "md", md: "lg" }}>{text}</Text>
     </Stack>
   );
 };
 
 const TrailProgramPage = () => {
-  const theme = useMantineTheme();
+  const isXXS = useMediaQuery(`(max-width: ${em(400)})`);
+  console.log("isXXS", isXXS);
   return (
     <>
       <Heading subtitle="LA GARA" title="PROGRAMMA" />
-      <Container fluid mt="xl">
-        <Flex>
+      <Container fluid className={classes.flexContainer}>
+        <Flex className={classes.flex}>
           <Container fluid>
-            <Stack align="center" gap="xl">
+            <Stack className={classes.stack}>
               <ProgramDayDate day="sabato" date="11/07/2024" />
               <HourScheduleGroup
                 hour="16:00 - 18:00"
                 text="Distribuzione pettorali"
               />
-              <Container mt={rem(160)}>
+              <Container className={classes.containerMountain}>
                 <MountainDrawing />
               </Container>
             </Stack>
           </Container>
           <Container fluid>
-            <Stack align="center" gap="xl">
+            <Stack className={classes.stack}>
               <ProgramDayDate day="domenica" date="12/07/2024" />
               <Container fluid className={classes.grid}>
                 <HourScheduleGroup
                   hour="06:30"
                   text="Distribuzione pettorali"
                 />
-                <Space />
+                {!isXXS && <Space />}
                 <HourScheduleGroup hour="08:15" text="Briefing pre-partenza" />
-                <Space />
+                {!isXXS && <Space />}
                 <HourScheduleGroup hour="08:30" text="Partenza" />
-                <Space />
+                {!isXXS && <Space />}
                 <HourScheduleGroup hour="10:45" text="Arrivo primi atleti" />
                 <HourScheduleGroup hour="12:30" text="Inizio pranzo" />
-                <Space />
+                {!isXXS && <Space />}
                 <HourScheduleGroup hour="14:00" text="Premiazioni" />
-                <Space />
+                {!isXXS && <Space />}
                 <HourScheduleGroup hour="14:15" text="Ultimi arrivi" />
-                <Text size="xl" fw={theme.other.fontWeights.semiBold}>
+                <Text
+                  fz={{ base: "md", sm: "lg", md: "xl" }}
+                  fw="var(--mantine-fw-semibold)"
+                >
                   ...MUSICA e BIRRA!
                 </Text>
-                <Space />
+                {!isXXS && <Space />}
               </Container>
             </Stack>
           </Container>

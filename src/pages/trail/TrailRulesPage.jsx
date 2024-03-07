2951fd1a48d84ad32ed8288e532e206659224867
elisabattistoni <elisa.battistoni.dev@gmail.com>
2024-03-07 15:03:13 +0100

    fix(imports): from trail registration and rules page

--- src/pages/trail/TrailRulesPage.jsx
+++ src/pages/trail/TrailRulesPage.jsx
@@ -1,10 +1,9 @@
-import { Container, Flex, Stack, Text, rem } from "@mantine/core";
+import { Stack, rem } from "@mantine/core";
 import Heading from "../../components/shared/Heading";
 import PageContainer from "../../components/shared/PageContainer";
 import PageSection from "../../components/shared/PageSection";
 import ParagraphText from "../../components/shared/ParagraphText";
 import HighlightedText from "../../components/shared/HighlightedText";
-import ButtonSecondary from "../../components/shared/ButtonSecondary";
 
 const TrailRulesPage = () => {
   return (
